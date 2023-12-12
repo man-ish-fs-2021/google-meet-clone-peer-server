2bd8c7927b0fd0d3f68a3b72ad1ccf7288ad4dd8
Manish Chanra <manishchandra58@gmail.com>
2023-12-12 21:00:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const { ExpressPeerServer } = require("peer");
+const app = express();
+
+app.enable("trust proxy");
+const isDev = app.settings.env === "development";
+const url = isDev ? "http://localhost:3000" : "";
+const PORT = process.env.PORT || 5050;
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
+app.use(cors({ origin: url }));
+
+const peerServer = ExpressPeerServer(server, {
+  path: "/",
+  corsOptions: { origin: url },
+  port: PORT,
+  key: "peerjs",
+});
+
+app.use("/", peerServer);
+
+module.exports = app;
